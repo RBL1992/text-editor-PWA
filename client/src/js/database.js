e1c96e53524d67b4354abb95ed5b63d6ea8dd54c
Roy <loganroyjr4@gmail.com>
2022-10-27 19:56:14 -0500

    revising getDB to display previous notes

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,8 +15,8 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log('PUT to the database');
-  const todosDb = await openDB('jate', 1);
-  const tx = todosDb.transaction('jate', 'readwrite');
+  const jate = await openDB('jate', 1);
+  const tx = jate.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
   const request = store.put({ content: content });
   const result = await request;
@@ -26,8 +26,8 @@ export const putDb = async (content) => {
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET from the database');
-    const todosDb = await openDB('jate', 1);
-    const tx = todosDb.transaction('jate', 'readonly');
+    const jate = await openDB('jate', 1);
+    const tx = jate.transaction('jate', 'readonly');
     const store = tx.objectStore('jate');
     const request = store.getAll();
     const result = await request;
